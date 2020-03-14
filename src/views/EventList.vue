7da46ce06b859aaaf12bb4705e27fb64c86ca95a
rahulkhimsuriya <rahulkhimsuriya38@gmail.com>
2020-03-14 19:24:21 +0530

    create dynamic route 🧭

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,5 +1,10 @@
 <template>
-  <h1>Events List</h1>
+  <div>
+    <h1>Events List</h1>
+    <router-link :to="{ name: 'event-show', params: { id: 1 } }"
+      >Show Event #1</router-link
+    >
+  </div>
 </template>
 
 <script>
