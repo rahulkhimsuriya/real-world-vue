f32dd49fe7aaeacf1c3be8a7f26e022e7eda0514
rahulkhimsuriya <rahulkhimsuriya38@gmail.com>
2020-03-15 15:13:01 +0530

    create state  🌟  and getter 🌠

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,9 +1,23 @@
 <template>
-  <h1>Event Create</h1>
+  <div>
+    <h1>Event Create, {{ user.name }}, {{ catLength }}</h1>
+    <p>this event created by {{ user.name }}</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {}
+import { mapState } from 'vuex'
+export default {
+  computed: {
+    catLength() {
+      return this.$store.getters.catLength
+    },
+    ...mapState(['user', 'categories'])
+  }
+}
 </script>
 
 <style></style>
